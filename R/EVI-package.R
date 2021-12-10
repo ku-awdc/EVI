c0b1da4a8219255adc523937fcc494ee77db72c9
kostas.pateras@gmail.com <63882794+kpateras@users.noreply.github.com>
2021-12-10 16:28:24 +0200

    update 0.9

--- R/EVI-package.R
+++ R/EVI-package.R
@@ -1,12 +1,12 @@
 #' @keywords internal
 "_PACKAGE"
 
-#' @import ggplot2
-#' @import cowplot
+
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+
 
 ## usethis namespace: end
 NULL
