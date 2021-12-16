94438f808ee947a3c80c1b1d919b5e6f197a7636
LefMel <emeletis@outlook.com>
2021-12-16 11:16:46 +0200

    update 1.1

--- R/Italy.R
+++ R/Italy.R
@@ -3,22 +3,11 @@
 #' Dataframe containing the first 150 days of Covid-19 pandemic 
 #' in Italy and the EVI analysis
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 150 rows and 3 variables:
 #' \describe{
 #'   \item{dates}{ID of date}
-#'   \item{Days}{Days from beginning of Covid-19 epidemic}
-#'   \item{ncases}{Number of observed cases}
-#'   \item{cum_ncases}{Cumulative number of observed cases}
-#'   \item{EVI}{Epidemic Volatility Index}
-#'   \item{Cases}{Moving average of number of cases}
-#'   \item{Index}{Positive or negative warning} 
-#'   \item{pvs}{Cumulative predictive value positive  of algorithm}
-#'   \item{pvn}{Cumulative predictive value negative  of algorithm}
-#'   \item{lag_all}{Current optimal lag at time (Days)}
-#'   \item{c_all}{Current optimal cut of point at time (Days)}
-#'   \item{se_all}{Cumulative sensitivity of algorithm}
-#'   \item{sp_all}{Cumulative specificity of algorithm}
-#'   \item{name}{Name of the Country}
+#'   \item{Cases}{Number of observed cases}
+#'   \item{Cum_Cases}{Cumulative number of observed cases}
 #' }
 #' @source \url{https://github.com/CSSEGISandData/COVID-19}
 "Italy"
