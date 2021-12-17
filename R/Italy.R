b19d8d435e056271648d47d78523fe6d1e4fa019
LefMel <emeletis@outlook.com>
2021-12-17 12:22:20 +0200

    Update Italy.R

--- R/Italy.R
+++ R/Italy.R
@@ -1,9 +1,9 @@
-#'  Cases of the first 150 days of the Covid-19 pandemic in Italy.
+#'  Cases of the first 149 days of the Covid-19 pandemic in Italy.
 #'
-#' A Dataframe containing the number of cases for the first 150 days of the Covid-19 pandemic 
+#' A Dataframe containing the number of cases for the first 149 days of the Covid-19 pandemic
 #' in Italy
 #'
-#' @format A data frame with 150 rows and 3 variables:
+#' @format A data frame with 149 rows and 3 variables:
 #' \describe{
 #'   \item{Date}{ID of date encoded with origin="01-01-1970"}
 #'   \item{Cases}{Number of newly observed cases per day}
