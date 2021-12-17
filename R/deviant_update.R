28d69fcedbb318d1c314285fd2107e0d8b43b919
kostas.pateras@gmail.com <63882794+kpateras@users.noreply.github.com>
2021-12-17 12:41:45 +0200

    update 1.2

--- R/deviant_update.R
+++ R/deviant_update.R
@@ -1,8 +1,8 @@
 #' This function is used after first running the deviant function
-#' 
-#' Once the deviant function has been used to analyze the already observed time series, 
-#' the deviant _update function is used to obtain the EVI output and estimates for the new cases that are recorded. 
-#' After running the deviant_update function the output of the deviant function is also updated with a new row of data for each nealy observed time point. 
+#'
+#' Once the deviant function has been used to analyze the already observed time series,
+#' the deviant _update function is used to obtain the EVI output and estimates for the new cases that are recorded.
+#' After running the deviant_update function the output of the deviant function is also updated with a new row of data for each nealy observed time point.
 
 
 #' @param new_cases the time series of the newly observed cases per unit of time (ideally per day)
@@ -19,53 +19,53 @@
 #' @export
 
 deviant_update=function(new_cases, cum = FALSE, r_a=7, r=0.2, lag_max=30){
-  source("mova.r")
-  source("medvol.r")
-  source("evi.r")
-  source("evifcut.r")
-  source("indic.r")
-  source("status.r")
-  source("rollsd.r")
-  
-  
+  #source("mova.r")
+  #source("medvol.r")
+  #source("evi.r")
+  #source("evifcut.r")
+  #source("indic.r")
+  #source("status.r")
+  #source("rollsd.r")
+
+
   start_cases=14
   lag_1=7
   c_1=0.01
   w_s =7
-  
-  
-  
-  
+
+
+
+
   if (cum == TRUE) new_cases = c(new_cases[1], diff(new_cases))
-  
-  
+
+
   #calculate the moving average of new confrimed cases
   cases=mova(new_cases,r_a)
-  
+
   roll=rollsd(cases[1:start_cases],lag_1)
   ev=evi(roll)
   ind=indic(ev,c_1, cases[1:start_cases])
   status=status(cases[1:start_cases],r)
-  
+
   #initiate chain for positive predictive value
   pvs=rep(NA, length(cases))
-  
+
   #initiate chain for negative predictive value
   pvn=rep(NA, length(cases))
-  
+
   lag_all=rep(NA, start_cases)
   c_all=rep(NA, start_cases)
-  
+
   se_all=rep(NA, start_cases)
   sp_all=rep(NA, start_cases)
-  
-  
+
+
   lag_all[1:start_cases]=lag_1
   c_all[1:start_cases]=c_1
-  
+
   diff= length(cases)-(nrow(EVI_output) +1)
   for (i in (nrow(EVI_output)+1): length(cases)){
-    
+
     case_t=cases[1:i]
     #case_t=cases[max(1,(i-33)):i]
     #lag_s=7
@@ -73,14 +73,14 @@ deviant_update=function(new_cases, cum = FALSE, r_a=7, r=0.2, lag_max=30){
     #lag_s=seq(lag_1,min(length(case_t),50), 1)
     c_s=seq(0.01,0.5, 0.01)
     #all_j=NA
-    
+
     all_lag=NA
     all_cut=NA
     all_se=NA
     all_sp=NA
-    
-    
-    
+
+
+
     for (j in lag_s){
       roll_t=rollsd(case_t,j)
       ev_t=evi(roll_t)
@@ -94,21 +94,21 @@ deviant_update=function(new_cases, cum = FALSE, r_a=7, r=0.2, lag_max=30){
         all_cut[[length(all_cut) + 1]] <- new_l
         all_se[[length(all_se) + 1]] <- new_se
         all_sp[[length(all_sp) + 1]] <- new_sp
-        
-        
+
+
       }
     }
-    
-    
-    
+
+
+
     sesp=as.data.frame(cbind(all_lag,all_cut,all_se,all_sp))
-    
-    
-    
-    
+
+
+
+
     #Select the row with the right window and cut
     index=which.max(sesp$all_se+sesp$all_sp-1)
-    
+
     #index=sesp[which(sesp$all_sp>0.80),]
     #index=which.max(index$all_se)
     #index=which(sesp$all_se==1 & sesp$all_sp>=0.95),1)
@@ -118,45 +118,45 @@ deviant_update=function(new_cases, cum = FALSE, r_a=7, r=0.2, lag_max=30){
     #   }
     #else
     #{index=which.max(sesp$all_se+sesp$all_sp-1)}
-    
-    
+
+
     #index=which(sesp$se>=0.6 & sesp$sp>0.9)
     print(i)
     print(sesp[index,])
-    
-    
-    
+
+
+
     #estimate the parameters for the last observed case
     lag_n=sesp$all_lag[index]
     c_n=sesp$all_cut[index]
-    
+
     roll_n=rollsd(cases[1:i],lag_n)
-    
+
     ev_n=evi(roll_n)
     ind_n=indic(ev_n,c_n, case_t)
     evicut_n=evifcut(ev_n, case_t, c_n, r)
-    
+
     roll=c(roll,roll_n[i])
     ev=c(ev,ev_n[i])
     ind=c(ind, ind_n[i])
-    
+
     lag_all=c(lag_all,lag_n)
     c_all=c(c_all,c_n)
-    
+
     se_all=c(se_all,all_se[index])
     sp_all=c(sp_all,all_sp[index])
-    
+
     pvs[i]=evicut_n$prev*all_se[index]/
       (evicut_n$prev*all_se[index]+(1-evicut_n$prev)*(1-all_sp[index]))
-    
+
     pvn[i]=(1-evicut_n$prev)*all_sp[index]/
       ((1-evicut_n$prev)*all_sp[index]+evicut_n$prev*(1-all_se[index]))
-    
-    
-    
+
+
+
   }
-  
-  
+
+
   Days=((length(cases)-diff):length(cases))
   EVI=ev[((length(ev)-diff):length(ev))]
   Cases=cases[((length(cases)-diff):length(cases))]
@@ -167,13 +167,13 @@ deviant_update=function(new_cases, cum = FALSE, r_a=7, r=0.2, lag_max=30){
   c_all=c_all[((length(c_all)-diff):length(c_all))]
   se_all=se_all[((length(se_all)-diff):length(se_all))]
   sp_all=sp_all[((length(sp_all)-diff):length(sp_all))]
-  
-    
+
+
   EVI_out_add=as.data.frame(cbind(Days, EVI, Cases, Index, pvs, pvn,
                               lag_all, c_all, se_all, sp_all))
 
   EVI_output=rbind(EVI_output,EVI_out_add)
-  
+
   EVI_output<<-(EVI_output)
-  
+
 }
